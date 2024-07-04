e02b75147435ee2827c8fd37f1fe4317b770b984
Grade <aborecevskij94@gmail.com>
2024-07-04 16:01:27 +0300

    upd: change endpoint method to post

--- first_task/urls.py
+++ first_task/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AddTaskView
 
 urlpatterns = [
-    path('add/<int:number1>/<int:number2>', AddTaskView.as_view({"get": "run_task"}), name='run_task'),
+    path('add/', AddTaskView.as_view({"post": "run_task"}), name='run_task'),
     path('add/result/<str:task_id>/', AddTaskView.as_view({"get": "get_task_result"}), name='task_result'),
 ]
--- first_task/views.py
+++ first_task/views.py
@@ -5,9 +5,12 @@ from celery.result import AsyncResult
 
 
 class AddTaskView(ViewSet):
-    def run_task(self, request, number1, number2):
-        result = add.delay(number1, number2)
-        return Response({"task_id": result.id})
+    def run_task(self, request):
+        data = request.data
+        number1 = data.get("number1")
+        number2 = data.get("number2")
+        task = add.delay(number1, number2)
+        return Response({"task_id": task.id})
 
     def get_task_result(self, request, task_id):
         task_result = AsyncResult(task_id)
